036aa1ee93b8e9eeba073e7692d36f2564b9e8c7
chenle <chen.le@aliyun.com>
2020-12-04 17:28:05 +0800

    首页部分代码更新

--- app/src/main/java/master/kotlin/readerpro/base/adapter/CommonRecyclerAdapter.kt
+++ app/src/main/java/master/kotlin/readerpro/base/adapter/CommonRecyclerAdapter.kt
@@ -1,6 +1,10 @@
 package master.kotlin.readerpro.base.adapter
 
 import android.content.Context
+import android.util.SparseArray
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate
 
@@ -22,7 +26,19 @@ abstract class CommonRecyclerAdapter<ITEM>(protected val  context: Context):Recy
         addItemViewDelegate(*delegates)
     }
 
+    private val  inflater:LayoutInflater = LayoutInflater.from(context)
+    private var headerItems:SparseArray<View>?=null
+    private var footerItems:SparseArray<View>?=null
 
+    private val itemDelegates:HashMap<Int,ItemViewDelegate<ITEM>> = hashMapOf()
+    private  val items:MutableList<ITEM> = mutableListOf()
+
+    private  val lock =Object()
+
+    private var itemClickListener: ((holder: ItemViewHolder, item: ITEM) -> Unit)? = null
+    private var itemLongClickListener: ((holder: ItemViewHolder, item: ITEM) -> Boolean)? = null
+
+    private var itemAnimation:ItemAnimation?=null
 
     fun <DELEGATE : ItemViewDelegate<ITEM>> addItemViewDelegate(delegate: DELEGATE) {
         itemDelegates[itemDelegates.size] = delegate
